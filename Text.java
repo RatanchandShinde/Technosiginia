6ad05cfc50e8b8e6ee2a3fc9735f6b72eca136e6
Ratanchand <vishuabhinav>
2023-03-23 18:06:03 +0530

    chang in ratanchand

--- Text.java
+++ Text.java
@@ -1,5 +1,8 @@
 public class simple
 {
   public static void main(String args[])
-  
-    sysytem .out.println("chang made by Ratanchand");
+  {
+    sysytem .out.println(chang by Ratanchand
+);
+  }
+}
