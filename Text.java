7d298bf44cc73bab9f00262e78f0a5675710ac8d
Ratanchand <vishuabhinav>
2023-03-23 18:17:01 +0530

    chang by rtrt

--- Text.java
+++ Text.java
@@ -2,7 +2,6 @@ public class simple
 {
   public static void main(String args[])
   {
-    sysytem .out.println(chang by Ratanchand
-);
-  };
+    sysytem .out.println(chang by Ratanchand shinde);
+  }
 }
